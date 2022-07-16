090afca0366cdca16db89d4ab9b363a15fbccbc9
Rahil Kaiser <kaiser.rahil@gmail.com>
2022-07-16 18:54:34 +0200

    removed gitmodule deep-motion

--- scripts/rigging.sh
+++ scripts/rigging.sh
@@ -1,7 +1,7 @@
 # echo("Renaming sample.obj to sample_ori.obj")
 # docker exec rignet mv /data/obj_file/sample.obj /data/obj_file/sample_ori.obj
 
-echo("Make sure file exists called sample_remesh.obj")
+# echo("Make sure file exists called sample_remesh.obj")
 docker exec rignet python quick_start.py
 
 # echo("Exporting as fbx file")
